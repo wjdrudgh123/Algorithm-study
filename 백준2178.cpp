2bd01cf17b3ac86e4e4c5870ffd455b2d8da4c60
Jung-Jaeho <wjdrudgh123@naver.com>
2019-07-04 21:59:41 +0900

    백준 문제 2178번 푸는 중

--- 백준2178.cpp
+++ 백준2178.cpp
@@ -1,30 +1,73 @@
 #include <iostream>
 #include <string>
 #include <queue>
+#include <vector>
+#include <utility>
 
 using namespace std;
 
-int n = 0;
-int m = 0;
-string maze[101];
-int result = 0;
-
-void solution(int i, int j){
-    maze[i][j] = '0';
-    result++;
-    if(i - 1 >= 0 && maze[i - 1][j] == '1') solution(i-1,j);
-    if(i + 1 < n && maze[i + 1][j] == '1') solution(i+1,j);
-    if(j - 1 >= 0 && maze[i][j -1] == '1') solution(i,j-1);
-    if(j + 1 < m && maze[i][j + 1] == '1') solution(i,j+1);
+int solution(string a[], int n, int m, int N, int M){
+    queue<pair<int,int>> q;
+    q.push(make_pair(n, m));
+    a[n][m] = '0';
+    int result = 0;
+    int goX = 0;
+    int goY = 0;
+    while(!q.empty()){
+        int x = q.front().first;
+        int y = q.front().second;
+        int lastY = q.back().second;
+        int temp = 0;
+        q.pop();
+        cout << x << "," << y << "," << q.size() << endl;
+        // if(y < lastY){
+        //     continue;
+        // }
+        result++;
+        if(x + 1 >= 0 && a[x+1][y] == '1'){
+            q.push(make_pair(x+1, y));
+            a[x+1][y] = '0';
+            temp++;
+            goX++;
+        }
+        if(y + 1 >= 0 && a[x][y + 1] == '1'){
+            q.push(make_pair(x, y + 1));
+            a[x][y+1] = '0';
+            temp++;
+            goY++;
+        }
+        if(y - 1 >= 0 && a[x][y-1] == '1'){
+            q.push(make_pair(x, y-1));
+            a[x][y-1] = '0';
+            temp++;
+        }
+        if(x - 1 >= 0 && a[x-1][y] == '1'){
+            q.push(make_pair(x-1, y));
+            a[x-1][y] = '0';
+            temp++;
+        }
+        if(x == N -1 && y == M - 1){
+            break;
+        }
+        if(temp == 0){
+            cout << "back" <<x <<","<< y<< endl;
+            result--;
+        }
+    }
+    cout << "out" <<result << ","<< goX << "," <<goY << endl;
+    return 0;
 }
 
 int main(){
+
+    int n = 0;
+    int m = 0;
+    string maze[101];
     cin >> n >> m;
     for(int i = 0; i < n; i++){
         cin >> maze[i];
     }
     //미로 완성
-    solution(0,0);
-    cout << result;
+    int result = solution(maze, 0, 0, n, m);
     return 0;
 }
